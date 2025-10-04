2492f8c04f6bb873fabd7642794d54a1d728c74e
Mykhailo <kurochkinmy@gmail.com>
2025-10-04 10:09:12 +0300

    feat: Implement full To-Do List application

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
-import React from 'react';
-import './App.scss';
-
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
-
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
+import React, { useState, useContext } from 'react';
+import { TodoList } from './components/TodoList';
+import { Footer } from './components/Footer';
+import { Header } from './components/Header';
+import { FilterType } from './types/FilterType';
+import { TodoContext } from './components/TodoContext';
 
 export const App: React.FC = () => {
+  const { hasTodos } = useContext(TodoContext);
+  const [filterType, setFilterType] = useState<FilterType>('all');
+
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
+
+      <div className="todoapp__content">
+        <Header />
+        <TodoList filterType={filterType} />
+        {hasTodos && (
+          <Footer filterType={filterType} onFilterTypeChange={setFilterType} />
+        )}
+      </div>
     </div>
   );
 };
